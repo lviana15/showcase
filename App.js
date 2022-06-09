82905921a1f339821beb3b7ee1283da054fd2399
lviana15 <lucasviana0604@gmail.com>
2022-06-09 05:19:26 -0300

    inicio projeto

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+
+import { List, MagnifyingGlass } from 'phosphor-react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+         <TouchableOpacity style={styles.icon}>
+            <List name='menu' size={32} color='#404354'/>
+         </TouchableOpacity>
+
+         <Text style={styles.title}>
+            Showcase
+         </Text>
+
+         <TouchableOpacity style={styles.icon}>
+            <MagnifyingGlass name='search' size={32} color="#404354" />
+         </TouchableOpacity>
+      </View>
+
+      <View style={styles.main}>
+         <Text>Hello World</Text>
+      </View>
+      <StatusBar backgroundColor="#B8BECC" barStyle="light-content"/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      backgroundColor: '#404354',
+   },
+   header: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+      height: 64,
+      padding: 8,
+      marginTop: 24,
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor: '#B8BECC'
+   },
+   main: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+   },
+   icon:{
+      marginHorizontal: 6
+   },
+   title:{
+      fontWeight: 'bold',
+      color: '#FF3CD3',
+      fontSize: 32
+   }
 });
