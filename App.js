62a35551dae0673001ceb095f7467d15568e9b59
lviana15 <lucasviana0604@gmail.com>
2022-06-14 09:24:18 -0300

    add navigation

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-import Home from './src/pages/Home';
-import Detail from './src/pages/Detail';
+import Routes from './src/routes';
 
 export default function App() {
   return (
    <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-         <Detail />
-         <StatusBar backgroundColor='#0C0C0C' barStyle='light-content' />
-      </SafeAreaView>
+      <Routes />
+      <StatusBar backgroundColor='#0C0C0C' barStyle='light-content' />
    </NavigationContainer>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: '#0C0C0C',
-   }
-});
---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+
+const Stack = createNativeStackNavigator();
+
+function Routes() {
+   return(
+      <Stack.Navigator>
+         <Stack.Screen
+            name='Home'
+            component={Home}
+            
+         />
+
+         <Stack.Screen
+            name='Detail'
+            component={Detail}
+         />
+      </Stack.Navigator>
+   )
+}
+
+export default Routes;
